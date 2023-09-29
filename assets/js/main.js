24ce882f9470bca4bc415a1a936103d2fa294cb0
Azhar <darkcoders0101@gmail.com>
2023-09-29 23:36:47 +0530

    adding html,css,js code

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,69 @@
 /*=============== SHOW MENU ===============*/
 
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
+
+/* ========== Menu Show ========= */
+/* Validate if constant exists */
+
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
+}
+
+/* ========== Menu Hidden ========= */
+/* Validate if constant exists */
+
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+    })
+}
+
 
 /*=============== REMOVE MENU MOBILE ===============*/
 
+const navLink = document.querySelectorAll('.nav__link');
+
+const linkAction = () =>{
+    const navMenu = document.getElementById('nav-menu')
+    // when we click on each nav__link we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*=============== SWIPER SHOE ===============*/
 
+const swiper = new Swiper('.home__swiper', {
+    loop: true,
+    spaceBetween: 32,
+    grabCursor: true,
+    effect: 'creative',
+    creativeEffect:{
+        prev:{
+            translate: [-100, 0, -500],
+            opacity: 0,
+        },  
+        next:{
+            translate: [100, 0, -500],
+            opacity: 0,
+        },  
+    },
+
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    }
+})
 
 /*=============== SHADOW HEADER ===============*/
+
+const shadowHeader = () =>{
+    const header = document.getElementById('header')
+    // when the scroll is greeater then 50 viewport height , add shadow-header class
+    this.scrollY >= 50 ? header.classList.add('shadow-header')
+                       : header.classList.remove('shadow-header')
+}
+window.addEventListener('scroll', shadowHeader)
